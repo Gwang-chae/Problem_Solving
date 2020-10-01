a82864db8e260f93975b7795aec703dd20e37264
Gwang-chae <kcseo25@naver.com>
2020-10-01 21:21:14 +0900

    implemantation

---
+++ 이것이 코딩테스트다(한빛)/상하좌우.py
@@ -0,0 +1,17 @@
+n = int(input())
+plan = list(map(str, input().split()))
+x,y = 1, 1
+
+for direction in plan :
+    if direction == 'L' and y != 1:
+        y -= 1
+    elif direction == 'R' and y != n:
+        y += 1
+    elif direction == 'U' and x != 1:
+        x -= 1
+    elif direction == 'D' and x != n:
+        x += 1
+    else :
+        pass
+
+print(x, y)
