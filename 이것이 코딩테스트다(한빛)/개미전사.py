fe97f8e3317f04bb5b64c48c71969a3fc885cbe2
Gwang-chae <kcseo25@naver.com>
2021-01-01 20:05:25 +0900

    DP practice

---
+++ 이것이 코딩테스트다(한빛)/개미전사.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+d = [0] * 101
+d[1] = array[0]
+d[2] = max(array[0], array[1])
+
+for i in range(3,n+1):
+    d[i] = max(d[i-1], d[i-2]+array[i-1])
+
+print(d)
+print(d[n])
